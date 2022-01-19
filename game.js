e8b1c7ab5e67a1e11c9b243cb9998d0c85eb2550
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-01-19 12:51:50 -0600

    check which button is pressed

--- game.js
+++ game.js
@@ -1,15 +1,25 @@
-var buttonColours=["red", "blue", "green", "yellow"]
-var gamePattern=[]
+var buttonColours = ["red", "blue", "green", "yellow"]
+var gamePattern = []
+var userClickedPattern = []
+
+//user button clicked
+$(".btn").click(function () {
+    var userChosenColour = $(this).attr("id")
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
+});
 
 
-function nextSequence(){
-    var randomNumber=Math.round((Math.random()*3));
-    var randomChosenColour=buttonColours[randomNumber];
+function nextSequence() {
+    var randomNumber = Math.round((Math.random() * 3));
+    var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour)
 
+    //adding sounds to buttons
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
     var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
     audio.play();
 }
+
+
 
