e1fad9ffc7e9fb236b62eeb89ac223c70f308aa5
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-01-19 13:27:21 -0600

    animate press

--- game.js
+++ game.js
@@ -7,6 +7,7 @@ $(".btn").click(function () {
     var userChosenColour = $(this).attr("id")
     userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
+    animatePress(userChosenColour);
 });
 
 
@@ -24,4 +25,9 @@ function nextSequence() {
 function playSound(name){
     var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
+}
+
+function animatePress(currentColour){
+    $("#"+currentColour).addClass("pressed");
+    setTimeout(function () {$("#"+currentColour).removeClass("pressed");}, 100);
 }
