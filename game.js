784ba19c6924e64dcd9985b32ce2c033aacee164
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-01-19 12:27:17 -0600

    adding sounds to buttons

--- game.js
+++ game.js
@@ -6,5 +6,10 @@ function nextSequence(){
     var randomNumber=Math.round((Math.random()*3));
     var randomChosenColour=buttonColours[randomNumber];
     gamePattern.push(randomChosenColour)
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
 }
 
